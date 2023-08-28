cf3f4d19f812c9e0781bfe1e01abbe3302895f0c
dopalescent <skudlgre000@gmail.com>
2023-08-28 17:02:05 -0400

    created helpers folder, added uuid.js and exported/imported to server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-// const api = require('./routes/index');
+const uuid = require('./helpers/uuid');
 const notesData = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
@@ -12,18 +12,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// app.use('/api', api);
-
-// app.get('*', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
-  // res.json(`${req.method} request received to get reviews`);
   console.info(`${req.method} request received to get reviews`);
   res.status(200).json(notesData);
 });
