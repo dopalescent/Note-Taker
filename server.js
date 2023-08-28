4b2b83dbada44eb1232af3240e814bace2f77f84
dopalescent <skudlgre000@gmail.com>
2023-08-28 14:10:24 -0400

    decided against using a routes folder, starting shift to run everything off of server.js file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index');
+const fs = require('fs');
+// const api = require('./routes/index');
+const notesData = require('./db/db.json');
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,10 +10,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
-app.use('/api', api);
+// app.use('/api', api);
+
+// app.get('/', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
